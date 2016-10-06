98eefa73da26f2a096c8e2aa5eb1dc8178845c68
Edgar <edgar.mantes11@gmail.com>
2016-10-05 20:10:14 -0500

    re-ordered the js file link

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 
-
 var questions = [
-	
+	new questionFactory("What does BMI stand for?", ['Body Max Index', 'Boyancy Mass Index', 'Body Mass Insert', 'Body Mass Index'], "Body Mass Index"),
 
 ]
+
