389ecb265a1ea00a31ad71144a4bcbf09c43c706
Edgar <edgar.mantes11@gmail.com>
2016-10-05 22:29:46 -0500

    Added text to the options and questions classes from the questions array

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 
 
-
-//render elements
 function Question(text, choices, answer){
 	return {
 		question: text,
 		choose: choices,
 		correct: answer
 	}
+
+}
+
+//remove-text function
+
+//render-elements function
+
+function renderHtml(object, element1, element2){
+	var questionItem = object.question;
+    $('ul').html('');
+	var itemsHTML = object.choose.map(function(item, index){
+		return '<li><button class="' + index + '">' + item + '</button></li>'
+	});
+	element1.append(itemsHTML);
+	element2.append(questionItem);
 
 }
 
@@ -19,10 +32,12 @@ function Question(text, choices, answer){
 //  Event Listeners
 function handleClicks(){
 	$('.container').on('click', '.start-button', function(event){
+		renderHtml(questions[0], $('.options'), $('.js-question-entry'));
 		$('.question, .options').removeClass('hidden');
 		$('.greeting-header, .greeting-button').addClass('hidden');
-		$('')
-	})
+	});
+
+	$('.option').on('click')
 
 }
 
