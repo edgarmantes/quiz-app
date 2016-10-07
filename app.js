410d27604210ae6c9b5ea1fec4790f2cbdd3af6b
Edgar <edgar.mantes11@gmail.com>
2016-10-06 21:17:05 -0500

    added event listener for cycling through all questions

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ function Question(text, choices, answer){
 
 function renderHtml(object, element1, element2){
 	var questionItem = object.question;
+	$('.js-question-entry').html('')
     $('ul').html('');
 	var itemsHTML = object.choose.map(function(item, index){
-		return '<li><button class="' + index + '">' + item + '</button></li>'
+		return '<li><button class="button ' + index + '">' + item + '</button></li>'
 	});
 	element1.append(itemsHTML);
 	element2.append(questionItem);
@@ -25,9 +26,25 @@ function renderHtml(object, element1, element2){
 }
 
 
+function rightAnswer(input){
+	alert("Good Job! " + input + " is correct!");
+	return;
+}
+
+function wrongAnswer(input, answer){
+	alert("Sorry, but " + input + " is incorrect. The correct answer is " + answer + ". Nice try though...");
+	return;
+}
 
 
+function renderScore (score){
 
+}
+
+
+function renderQuestNum (index){
+	
+}
 
 //  Event Listeners
 function handleClicks(){
@@ -37,7 +54,25 @@ function handleClicks(){
 		$('.greeting-header, .greeting-button').addClass('hidden');
 	});
 
-	$('.option').on('click')
+	var score = 0;
+	var questionIndex = 0;
+	$('ul.options').on('click', '.button', function (event){
+	
+		var input = $(this).text();
+		var answer = questions[questionIndex].correct;
+		questionIndex++;
+		if ( input === answer && questions.length > questionIndex) {
+			rightAnswer(input);
+			renderHtml(questions[questionIndex], $('.options'), $('.js-question-entry'));
+			score++;
+		} else if ( input !== answer && questions.length > questionIndex){
+			wrongAnswer(input, answer);
+			renderHtml(questions[questionIndex], $('.options'), $('.js-question-entry'));
+		} else {
+			alert("DONE!!!! How did you think you did? ")
+		}
+		console.log(score);
+	})
 
 }
 
